31fd4d6c6fc8ef03e5ef2ab79043e97a658605fb
C0B23008 <c0b23008ff@edu.teu.ac.jp>
2024-04-16 18:17:41 +0900

    課題3

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -16,28 +16,33 @@ def main():
     kk_rct.center = 300, 200
     bg_img2 = pg.transform.flip(bg_img, True, False)
     tmr = 0
-    kk_rct.center = 300, 200
+    a = 0
+    b = 0
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_lst = pg.key.get_pressed()
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0, -1))
+            b = - 1
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0, 1))
+            b =  1
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1, 0))
+            a = - 1
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((2, 0))
+            a = 2
         else:
-            kk_rct.move_ip((-1, 0))
+            a = - 1
         x = tmr%3200 #練習6
         screen.blit(bg_img, [-x, 0]) #練習6
         screen.blit(bg_img2, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
         screen.blit(bg_img2, [-x+4800, 0])
+        kk_rct.move_ip((a, b))
         screen.blit(kk_img, kk_rct) #練習4
         pg.display.update()
+        a = 0
+        b = 0
         tmr += 1        
         clock.tick(200) #練習5
 
